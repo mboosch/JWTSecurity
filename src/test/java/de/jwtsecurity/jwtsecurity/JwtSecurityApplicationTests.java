dd544865f86da65330139be5fa8a05d693ca7377
matthias <matthias_boosch@yahoo.de>
2023-06-16 15:13:25 +0200

    init: big bang

---
+++ src/test/java/de/jwtsecurity/jwtsecurity/JwtSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jwtsecurity.jwtsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
